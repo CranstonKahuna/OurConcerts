ecc0c0d2a518c0fb14dd3a5f18f84d05340b0bfd
Dean Thomas <deant@dean-thomass-macbook-pro.local>
2017-09-28 12:48:20 -0700

    Clean up: Remove tools from ToolsVC.swift

--- OurConcerts/ToolsVC.swift
+++ OurConcerts/ToolsVC.swift
@@ -11,13 +11,10 @@ import MobileCoreServices
 
 class ToolsVC: UIViewController, UIDocumentPickerDelegate, UINavigationControllerDelegate {
     
-    var fileManager = FileManager()
     var tmpDir = NSTemporaryDirectory() as String
-    let fileName = "sample.txt"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -35,81 +32,6 @@ class ToolsVC: UIViewController, UIDocumentPickerDelegate, UINavigationControlle
         // Pass the selected object to the new view controller.
     }
     */
-
-    @IBAction func createFile(_ sender: UIButton) {
-        let path = NSString(string: tmpDir).appendingPathComponent(fileName)
-        let contentsOfFile = "Sample Text"
-        
-        // Write File
-        do {
-            try contentsOfFile.write(toFile: path, atomically: true, encoding: String.Encoding.utf8)
-            print("File sample.txt created at tmp directory")
-        } catch {
-            print("Failed to create file")
-
-            let error = error as NSError
-            print("\(error)")
-        }
-    }
-    
-    @IBAction func listDirectory(_ sender: UIButton) {
-        // List Content of Path
-        let isFileInDir = enumerateDirectory() ?? "Empty"
-        print("Contents of Directory =  \(isFileInDir)")
-    }
-    
-    @IBAction func viewFileContent(_ sender: UIButton) {
-        let isFileInDir = enumerateDirectory() ?? ""
-        
-//        let path = tmpDir.stringByAppendingPathComponent(isFileInDir)
-        let path = NSString(string: tmpDir).appendingPathComponent(isFileInDir)
-
-//        let contentsOfFile = NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding, error: nil)
-        do {
-            let content = try String(contentsOfFile: path)
-            print("Content of file = \(content)")
-        } catch {
-            print("No file found")
-        }
-    }
-    
-    @IBAction func deleteFile(_ sender: UIButton) {
-        
-        if let isFileInDir = enumerateDirectory() {
-            let path = NSString(string: tmpDir).appendingPathComponent(isFileInDir)
-            do {
-                try fileManager.removeItem(atPath: path)
-            } catch {
-                print("Failed to delete file")
-                let error = error as NSError
-                print("\(error)")
-            }
-        }
-    }
-    
-    func enumerateDirectory() -> String? {
-//        let filesInDirectory =  fileManager.contentsOfDirectoryAtPath(tmpDir, error: &error) as? [String]
-        do {
-            let files = try fileManager.contentsOfDirectory(atPath: tmpDir)
-            if files.count > 0 {
-                print("eD: \(files)")
-                for file in files {
-                    if file == fileName {
-                        print("sample.txt found")
-                        return file
-                    }
-                }
-                print("File not found")
-                return nil
-            } else {
-                print("eD: directory empty")
-            }
-        } catch {
-            let error = error as NSError
-            print("\(error)")
-        }
-        return nil
-    }
     
     // MARK: Document Picker Functions
 
@@ -117,18 +39,12 @@ class ToolsVC: UIViewController, UIDocumentPickerDelegate, UINavigationControlle
         let importPicker = UIDocumentPickerViewController(documentTypes: [kUTTypeItem as String], in: .import)
         importPicker.delegate = self
         importPicker.modalPresentationStyle = .formSheet
-
 //        importPicker.addOption(withTitle: "iPhone", image: nil, order: .first) {
 //            print("In addOption")
 //        }
         self.present(importPicker, animated: true, completion: nil)
     }
-    
-    struct concertFromJson: Codable {
-        var Date: String
-        var BSName: String
-    }
-    
+
     @available(iOS 8.0, *)
     public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentAt url: URL) {
         let fn = url.lastPathComponent
