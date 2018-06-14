4685175dc6e699d5a82cb2f21a6e279ac4688315
Dean Thomas <deant@iname.com>
2018-06-14 13:22:24 -0700

    Fix trivial typo

--- OurConcerts/RatingControl.swift
+++ OurConcerts/RatingControl.swift
@@ -79,7 +79,7 @@ import UIKit
            return
         }
         guard let index = ratingButtons.index(of:button) else {
-            fatalError("The button, \(button) is not in the ratinButtons array: \(ratingButtons)")
+            fatalError("The button, \(button) is not in the ratingButtons array: \(ratingButtons)")
         }
         // Calculate the rating of the selected button
         rating = Int16(index)
